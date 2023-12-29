ce9b9f808cc8e4be3f45465cb12c164db98941a5
ice-wiz <img_2020012@iiitm.ac.in>
2023-12-30 00:33:04 +0530

    no progress

--- hi.js
+++ hi.js
@@ -1,22 +1,39 @@
-// // // // Setting an environment variable
-// // // process.env.GITHUB_ID = 'Iv1.d28a078e418af673';
-// // // // GITHUB_ID="Iv1.d28a078e418af673"
-// // // // Accessing the environment variable
-// // // const GITHUB_ID = process.env.GITHUB_ID;
-// // // console.log(GITHUB_ID);
+// const GITHUB_ID = "Iv1.d28a078e418af673";
+// const GITHUB_SECRET = "2816864ee8753023affc4481bffe0290aeabdd83";
+// const NEXTAUTH_URL = "http://localhost:3000/";
+// const NEXTAUTH_URLSECRET = "83b639984fdc723ac9d1021b17167d12";
 
+// const saveEnv = (name, value) => {
+//     process.env[name] = value;
+//     let x = process.env[name];
+//     console.log(x);
+// }
 
+// const array = ['GITHUB_ID', 'GITHUB_SECRET', 'NEXTAUTH_URL', 'NEXTAUTH_URLSECRET'];
 
+// for (const val of array) {
+//     saveEnv(val, eval(val));
+// }
 
-// // process.env.GITHUB_SECRET="MIIEpAIBAAKCAQEAuvgKIi+LhM+teqIE+kKmTZZKrSARqzDRm6L5AL4WkIhRu9gvieE0eTjkHk3v7zGbDS+3zQbubC88vZi4M/7Pu/sm+n7Z0XOL28mKPCsdcOyeDuJxvSDALPjLNiCxxPBX9oSygsz+PTa+bYjY53co5/kAbVK2Fj2aV6ljKpKHh8+4pmz32+ak3Yo0EdeIr7T9JikZeG4fgxWxSAP1NoSzSfx2SRi6mGEM4tHDk/OvvZgbmhOkdoMFYqnBuwsCFpIyxw0Pfqfrd2Y1OoQQJZvPKr6TUnwgY+5c9EQkknrqDU+ZrfYauu1WRezcY0Vy3PcLzgtNYo3/s15WV1KgAYSs3wIDAQABAoIBAG+1CrjqFz2+pzOB4d90OtBYXz2Yz3WssU9/y6355JluGQdWXvDOMP/MUd8ZSaVryevS+snwvjq87lmJCWVndM4nfF5ls9ZEZudmPsU8LVbuyHC0MqiBDNDRxPgOCKhVwPh0yAMGX1BLiPMAtDd3JmWSjALFTC11mOWPOsSQz5EAMIJhY5O6A1ZoU9WDI6v0jPQBKSbipHCn/rAdvawumurza+vusu3nip0Ulpvqs+QHcoRXGVKaKkQbCjk0apn11O8kxfxd8r0F8JRiqLWYsTy8I+I5og3VqwJkfiWC6pMIyTsPVFYa5fbcuRPobWOgJJ2yvEJ0Zs+NSrNltzh4i6ECgYEA6yi7L+xLecvJ3N6Xnd7OmQkn4tJTXEJZGpcw2Z4quZsOgO5t+dGKy+GA/yMvQWE2vV28F0U6zvaDdZbeDvCQUxqJI+qgDmBjIfLAconMfOWtnyir9fY2bbmlVkVlMLSCUVM56gUs65WzA0AAwrCzI4onV+fDYEqNuIhWFz9PCCUCgYEAy4n5MFOgWjfiAm4fxNl57wB8RrteB5DjnIWXyKe6Fs5mqAcK5P411dttP6ocESoELC67hN14Cd7/8ZN13L86V7r+A/8fbqhVST4RGKa0u4wwq/67MK6ub5TmTSaBBjhuHU9ANWG7G4Pu8yeZhCIHpuZZhkzxbHdNZerp8wZ3n7MCgYEAnywKx/1heSd/k4j3h1xw6xVPcjB7TE98kIsV/bcZpFhYEN1Sg25aTHZvIkNmkRKEQtlXn0i07bHeeZGM7P/YMOgdO7psbE7ByxDNQroTN9cfFkQpYWsunEa0qxh9zeHg87sp0EWAilXhvals2ltQZhX1W4L8tAjCtM1TuJiZniECgYEAre6l50pPSUlf9butokZGEJtVmAwdelGyb9sprerG9x5CU2xRPwaNcsckQLdZbMF+91KoJA4tuiG6jfH5Uj6H2NiOoVOuxJ1M5F4JtgsffvV50kDILf3YSrnvZBZ3Q52NW7aRpAx0HDtOQrOgWG3MJC/MaRvvY0U/+swYR/lK2EkCgYBHG4jqrlPt9gEedquMaOMpap42UFc0iTYja/OjfWgaVU25KmsYSScr9jr/RcHuhDWuAu+v+1wetnq82pLPZKcsVxBdv/CmSnOpebG3OxFvW60E4E0AAt9a6qdFckGHuxoRn8J+85NHzGTSAprXQ53DSoXDQ0SihOQXDT7tuxCJ9w=="
+const GITHUB_ID = "Iv1.d28a078e418af673";
+const GITHUB_SECRET = "2816864ee8753023affc4481bffe0290aeabdd83";
+const NEXTAUTH_URL = "http://localhost:3000/";
+const NEXTAUTH_URLSECRET = "83b639984fdc723ac9d1021b17167d12";
 
-// // const GITHUB_SECRET = process.env.GITHUB_SECRET;
-// //  console.log(GITHUB_SECRET);
+const saveEnv = (name, value) => {
+  process.env[name] = value;
+  let x = process.env[name];
+  console.log(x);
+};
 
-// process.env.NEXTAUTH_URLSECRET="83b639984fdc723ac9d1021b17167d12"
-// const hello=process.env.NEXTAUTH_URLSECRET;
-// console.log(hello);
+const array = [
+  ['GITHUB_ID', GITHUB_ID],
+  ['GITHUB_SECRET', GITHUB_SECRET],
+  ['NEXTAUTH_URL', NEXTAUTH_URL],
+  ['NEXTAUTH_URLSECRET', NEXTAUTH_URLSECRET],
+];
 
-process.env.GITHUB_ID = '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';
-const dd=process.env.GITHUB_ID
-console.log(dd)
+for (const [name, value] of array) {
+  saveEnv(name, value);
+}
+
